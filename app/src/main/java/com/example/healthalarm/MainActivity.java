68bfd518eff2167905a6643ad644aba0b82fe59a
hamza94max <hamzagamal854@gmail.com>
2021-06-06 19:51:54 +0200

    time counting

--- app/src/main/java/com/example/healthalarm/MainActivity.java
+++ app/src/main/java/com/example/healthalarm/MainActivity.java
@@ -9,36 +9,66 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    String [] Btn_texts = {"Stop ", "Start"};
+    int count = 0;
+    TextView btn ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.start_btn);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void start_btn(View view) {
+        count ++ ;
 
-        new CountDownTimer(30000, 1000) {
+        if (count % 2 == 0){
+            btn.setText(Btn_texts[1]);
+        }else {
+            btn.setText(Btn_texts[0]);
+        }
 
-            public void onTick(long millisUntilFinished) {
-                Toast.makeText(MainActivity.this, "seconds remaining: " + millisUntilFinished / 1000, Toast.LENGTH_SHORT).show();
-            }
-
-            public void onFinish() {
-                Toast.makeText(MainActivity.this, "done!", Toast.LENGTH_SHORT).show();
-            }
-        }.start();
+        TextView remainingtext = findViewById(R.id.rmd_text);
 
 
 
+
+
+            new CountDownTimer( 10 * 1000, 1000) {
+
+
+                public void onTick(long millisUntilFinished) {
+
+                    int minutes = (int) (millisUntilFinished / (60 * 1000));
+                    int seconds = (int) ((millisUntilFinished / 1000) % 60);
+                    String str = String.format("%d:%02d", minutes, seconds);
+                    remainingtext.setText("Time remaining: " + str);
+                }
+
+                public void onFinish() {
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // code that recount the time
+                            Toast.makeText(MainActivity.this, "done!", Toast.LENGTH_SHORT).show();
+                        }
+                    }, 3000);
+                }
+            }.start();
 
 
 
