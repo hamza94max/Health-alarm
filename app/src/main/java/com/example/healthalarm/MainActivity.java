4880c29eb37d2f91ef0921be4abb9559136ba334
hamza94max <hamzagamal854@gmail.com>
2021-06-07 16:13:33 +0200

    funcation

--- app/src/main/java/com/example/healthalarm/MainActivity.java
+++ app/src/main/java/com/example/healthalarm/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.healthalarm;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,18 +9,13 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
-
-import java.lang.reflect.Array;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    String [] Btn_texts = {"Stop ", "Start"};
-    int count = 0;
-    TextView btn ;
+    String [] Btn_texts = {"Stop ", "Start"} ;
+    int count = 0 ;
+    TextView btn , remainingtext ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.start_btn);
+        remainingtext = findViewById(R.id.rmd_text);
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -37,40 +33,46 @@ public class MainActivity extends AppCompatActivity {
 
         if (count % 2 == 0){
             btn.setText(Btn_texts[1]);
+            remainingtext.setVisibility(View.INVISIBLE);
+
         }else {
             btn.setText(Btn_texts[0]);
+
         }
-
-        TextView remainingtext = findViewById(R.id.rmd_text);
-
-
-
-
-
-            new CountDownTimer( 10 * 1000, 1000) {
-
-
-                public void onTick(long millisUntilFinished) {
-
-                    int minutes = (int) (millisUntilFinished / (60 * 1000));
-                    int seconds = (int) ((millisUntilFinished / 1000) % 60);
-                    String str = String.format("%d:%02d", minutes, seconds);
-                    remainingtext.setText("Time remaining: " + str);
-                }
-
-                public void onFinish() {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            // code that recount the time
-                            Toast.makeText(MainActivity.this, "done!", Toast.LENGTH_SHORT).show();
-                        }
-                    }, 3000);
-                }
-            }.start();
-
-
+        StartTime();
 
     }
+
+    private void StartTime (){
+
+        new CountDownTimer( 10 * 1000, 1000) {
+
+
+            @SuppressLint("SetTextI18n")
+            public void onTick(long millisUntilFinished) {
+
+                int minutes = (int) (millisUntilFinished / (60 * 1000));
+                int seconds = (int) ((millisUntilFinished / 1000) % 60);
+                @SuppressLint("DefaultLocale")
+                String str = String.format("%d:%02d", minutes, seconds);
+                remainingtext.setText( getString(R.string.Timeremaining)+ str);
+            }
+
+            public void onFinish() {
+                delay(3);}
+
+        }.start();}
+
+
+    private void delay (int seconds){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // code that recount the time
+                Toast.makeText(MainActivity.this, "done!", Toast.LENGTH_SHORT).show();
+            }
+        }, seconds*1000);
+    }
+
 }
